7caf76a25295c595569c1ddd72e4c3739af14e9e
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-06-14 18:01:05 +0100

    modify

--- tests/utils/redis.test.js
+++ tests/utils/redis.test.js
@@ -1,18 +1,35 @@
-const redisClient = require('../redisClient');
+/* eslint-disable import/no-named-as-default */
+import { expect } from 'chai';
+import redisClient from '../../utils/redis';
 
-describe('redisClient', () => {
-  afterAll(() => {
-    redisClient.quit();
+describe('+ RedisClient utility', () => {
+  before(function (done) {
+    this.timeout(10000);
+    setTimeout(done, 4000);
   });
 
-  test('should connect to Redis', async () => {
-    await redisClient.connect();
-    expect(redisClient.isOpen).toBe(true);
+  it('+ Client is alive', () => {
+    expect(redisClient.isAlive()).to.equal(true);
   });
 
-  test('should set and get a key-value pair', async () => {
-    await redisClient.set('key', 'value');
-    const value = await redisClient.get('key');
-    expect(value).toBe('value');
+  it('+ Setting and getting a value', async function () {
+    await redisClient.set('test_key', 345, 10);
+    expect(await redisClient.get('test_key')).to.equal('345');
+  });
+
+  it('+ Setting and getting an expired value', async function () {
+    await redisClient.set('test_key', 356, 1);
+    setTimeout(async () => {
+      expect(await redisClient.get('test_key')).to.not.equal('356');
+    }, 2000);
+  });
+
+  it('+ Setting and getting a deleted value', async function () {
+    await redisClient.set('test_key', 345, 10);
+    await redisClient.del('test_key');
+    setTimeout(async () => {
+      console.log('del: test_key ->', await redisClient.get('test_key'));
+      expect(await redisClient.get('test_key')).to.be.null;
+    }, 2000);
   });
 });
