a096b75be8344b1271d6a5d5d0e390cf7b264534
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-06-14 17:48:38 +0100

    modify

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,54 +1,19 @@
-const { MongoClient, ObjectId } = require('mongodb');
+/* eslint-disable import/no-named-as-default */
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-class DBClient {
-  constructor() {
-    this.client = new MongoClient(process.env.DB_HOST || 'mongodb://localhost:27017');
-    this.db = null;
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
   }
 
-  async connect() {
-    if (this.db) return this.db;
-    await this.client.connect();
-    this.db = this.client.db('files_manager');
-    return this.db;
-  }
-
-  async isAlive() {
-    try {
-      const db = await this.connect();
-      await db.command({ ping: 1 });
-      return true;
-    } catch (error) {
-      console.error('Error checking DB connection:', error);
-      return false;
-    }
-  }
-
-  async nbUsers() {
-    const db = await this.connect();
-    const usersCollection = db.collection('users');
-    return await usersCollection.countDocuments();
-  }
-
-  async nbFiles() {
-    const db = await this.connect();
-    const filesCollection = db.collection('files');
-    return await filesCollection.countDocuments();
-  }
-
-  async getUserByEmail(email) {
-    const db = await this.connect();
-    const usersCollection = db.collection('users');
-    return await usersCollection.findOne({ email });
-  }
-
-  async createUser(email, hashedPassword) {
-    const db = await this.connect();
-    const usersCollection = db.collection('users');
-    const result = await usersCollection.insertOne({ email, password: hashedPassword });
-    return await usersCollection.findOne({ _id: result.insertedId });
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
   }
 }
-
-const dbClient = new DBClient();
-module.exports = dbClient;
